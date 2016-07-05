11358c7ed793b79e2e6368ea8079f6de4d95ecc5
EMG114 <egutierrez411@gmail.com>
2016-07-04 20:14:34 -0400

    done

---
+++ FlashyTableviews/FlashyTableviews/FlashyTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  FlashyTableViewController.h
+//  FlashyTableviews
+//
+//  Created by Erica on 6/30/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import "SWTableViewCell.h"
+
+@interface TableViewController : UITableViewController <SWTableViewCellDelegate>
+@end
---
+++ FlashyTableviews/FlashyTableviews/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FlashyTableviews
+//
+//  Created by Erica on 6/30/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
